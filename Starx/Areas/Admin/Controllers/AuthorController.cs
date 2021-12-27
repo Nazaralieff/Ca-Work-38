55938db3ed7dec3553d2b09fce9cea03269a5a6a
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-28 00:45:13 +0400

    Works are done

---
+++ Starx/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Starx.Areas.Admin.ViewModel;
+using Starx.Data;
+using Starx.Models;
+using System.Linq;
+
+namespace Starx.Areas.Admin.Controllers
+{
+    [Area("admin")]
+
+    public class AuthorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+
+
+        public AuthorController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmAuthor model = new VmAuthor()
+            {
+                authors = _context.authors.ToList(),
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(VmAuthor model)
+        {
+            _context.authors.Add(model.author);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int Id)
+        {
+            // Version 1
+            //Author FoundAuthor = _context.authors.FirstOrDefault(e => e.Id == Id);
+            //_context.authors.Remove(FoundAuthor);
+
+
+            //Version 2
+            _context.authors.Remove(_context.authors.FirstOrDefault(e => e.Id == Id));
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int Id)
+        {
+            VmAuthor model = new VmAuthor()
+            {
+                authors = _context.authors.ToList(),
+                update = _context.authors.FirstOrDefault(e => e.Id == Id)
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(VmAuthor model)
+        {
+            Author UpdatedAuthor = model.update;
+            _context.authors.FirstOrDefault(e => e.Id == UpdatedAuthor.Id).Name = UpdatedAuthor.Name;
+            _context.authors.FirstOrDefault(e => e.Id == UpdatedAuthor.Id).Surname = UpdatedAuthor.Surname;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Starx/Models/Service.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Starx.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [MaxLength(200)]
+        public string Tittle { get; set; }
+
+
+
+        [Column(TypeName = "ntext")]
+        public string Content { get; set; }
+
+
+
+        [MaxLength(500)]
+        public string CoverImg { get; set; }
+
+
+        [NotMapped]
+        public IFormFile ImageFile{ get; set; }
+
+
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+
+
+        public DateTime CreateTime { get; set; }
+
+
+
+
+
+    }
+}
